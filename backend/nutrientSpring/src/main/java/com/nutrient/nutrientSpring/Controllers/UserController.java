458fe5c7b9b2750bad17f96eea8f177bc3a66f44
MarinaOct <marinaoctober98@gmail.com>
2019-08-30 02:04:40 +0300

    fix repos and ctrl

--- backend/nutrientSpring/src/main/java/com/nutrient/nutrientSpring/Controllers/UserController.java
+++ backend/nutrientSpring/src/main/java/com/nutrient/nutrientSpring/Controllers/UserController.java
@@ -1,25 +1,23 @@
 package com.nutrient.nutrientSpring.Controllers;
 
 import com.nutrient.nutrientSpring.Model.User.User;
+import com.nutrient.nutrientSpring.Repos.User.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@RequestMapping(path="/api")
-public class GroupController {
+@RequestMapping("/api/users")
+public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping(path="/users")
-    public @ResponseBody Iterable<User> getAllUsers() {
+    @GetMapping
+    public Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 }
--- backend/nutrientSpring/src/main/java/com/nutrient/nutrientSpring/Repos/User/UserRepository.java
+++ backend/nutrientSpring/src/main/java/com/nutrient/nutrientSpring/Repos/User/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
